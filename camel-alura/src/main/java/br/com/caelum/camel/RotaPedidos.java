0928e3046c0e11bfaf13141fed5385f5508ccb34
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-06-24 01:43:29 -0300

    Exemplo que copia arquivos de uma pasta para outra.

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -8,6 +9,21 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
+		
+		context.addRoutes(new RouteBuilder() {
 
-	}	
+            @Override
+            public void configure() throws Exception {
+            	from("file:pedidos?delay=5s&noop=true").
+            	log("${id} - ${body}").
+                to("file:saida");
+            	//link documentacao: http://camel.apache.org/file2.html
+            }
+            
+		});
+		
+		context.start(); //aqui camel realmente começa a trabalhar
+        Thread.sleep(2000); //esperando um pouco para dar um tempo para camel
+
+	}
 }

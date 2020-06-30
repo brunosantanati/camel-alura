8ddafa8839efaefe6b8aa0a64a140763a7decf34
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-06-30 09:10:04 -0300

    Manter Camel rodando ate apertar ctrl + c.

--- desafio-1/src/main/java/br/com/caelum/camel/desafio1/rota/RotaHttpPollingNegociacoes.java
+++ desafio-1/src/main/java/br/com/caelum/camel/desafio1/rota/RotaHttpPollingNegociacoes.java
@@ -1,9 +1,8 @@
 package br.com.caelum.camel.desafio1.rota;
 
-import org.apache.camel.CamelContext;
+import org.apache.camel.Main;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.xstream.XStreamDataFormat;
-import org.apache.camel.impl.DefaultCamelContext;
 
 import com.thoughtworks.xstream.XStream;
 
@@ -11,33 +10,45 @@ import br.com.caelum.camel.desafio1.model.Negociacao;
 
 public class RotaHttpPollingNegociacoes {
 	
-	public static void main(String[] args) throws Exception {
-		
-		final XStream xStream = new XStream();
-		xStream.alias("negociacao", Negociacao.class);
-		
-		CamelContext context = new DefaultCamelContext();
-		
-		context.addRoutes(new RouteBuilder() {
+    private Main main;
 
-            @Override
-            public void configure() throws Exception {
-            	from("timer://negociacoes?fixedRate=true&delay=1s&period=5s").
-	                to("http4://argentumws-spring.herokuapp.com/negociacoes").
-	                    convertBodyTo(String.class).
-	                    unmarshal(new XStreamDataFormat(xStream)).
-	                    split(body()).
-	                    log("${body}").
-	            end();
-            	//setHeader(Exchange.FILE_NAME, constant("negociacoes.xml")).
-                //to("file:saida");
-            }
-            
-		});
-		
-		context.start();
-        Thread.sleep(20000);
-		
-	}
+    public static void main(String[] args) throws Exception {
+    	RotaHttpPollingNegociacoes example = new RotaHttpPollingNegociacoes();
+        example.boot();
+    }
+
+    public void boot() throws Exception {
+        // create a Main instance
+        main = new Main();
+        // enable hangup support so you can press ctrl + c to terminate the JVM
+        main.enableHangupSupport();
+        // add routes
+        main.addRouteBuilder(new MyRouteBuilder());
+
+        // run until you terminate the JVM
+        System.out.println("Starting Camel. Use ctrl + c to terminate the JVM.\n");
+        main.run();
+    }
+
+    private static class MyRouteBuilder extends RouteBuilder {
+        @Override
+        public void configure() throws Exception {
+        	
+    		final XStream xStream = new XStream();
+    		xStream.alias("negociacao", Negociacao.class);
+        	
+        	from("timer://negociacoes?fixedRate=true&delay=1s&period=5s").
+            to("http4://argentumws-spring.herokuapp.com/negociacoes").
+                convertBodyTo(String.class).
+                unmarshal(new XStreamDataFormat(xStream)).
+                split(body()).
+                log("${body}").
+            end();
+	    	//setHeader(Exchange.FILE_NAME, constant("negociacoes.xml")).
+	        //to("file:saida");
+        }
+    }
+
+    //documentation: https://people.apache.org/~dkulp/camel/running-camel-standalone-and-have-it-keep-running.html
 
 }

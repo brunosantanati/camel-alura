25ef0cb800d1f41baa334520b1984a690a7214b4
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-07-05 15:21:41 -0300

    Mostrar o stacktrace com a excecao no log.

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -18,16 +18,17 @@ public class RotaPedidos {
             public void configure() throws Exception {
             	errorHandler(
            		    deadLetterChannel("file:erro").
-           		    	maximumRedeliveries(3).
-           		    		redeliveryDelay(1000).
-           		    		onRedelivery(new Processor() {            
-	           		             @Override
-	           		             public void process(Exchange exchange) throws Exception {
-	           		            	int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
-	           		            	int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
-	           		             	System.out.println("Redelivery - " + counter + "/" + max );
-	           		             }
-           		    		})
+           		    logExhaustedMessageHistory(true).
+           		    maximumRedeliveries(3).
+           		    redeliveryDelay(1000).
+           		    onRedelivery(new Processor() {            
+						@Override
+						public void process(Exchange exchange) throws Exception {
+							int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+							int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+							System.out.println("Redelivery - " + counter + "/" + max );
+						}
+           		    })
             	);
             	from("file:pedidos?delay=5s&noop=true").
             		log("${file:name}"). //logando nome do arquivo

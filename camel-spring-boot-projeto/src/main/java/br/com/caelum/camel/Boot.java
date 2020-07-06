726bebea5de1c6e30d13ce40984d0f4b921e3869
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-07-06 01:00:42 -0300

    Projeto Spring Boot.

---
+++ camel-spring-boot-projeto/src/main/java/br/com/caelum/camel/Boot.java
@@ -0,0 +1,11 @@
+package br.com.caelum.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot {
+	public static void main(String[] args) {
+		SpringApplication.run(Boot.class, args);
+	}
+}
